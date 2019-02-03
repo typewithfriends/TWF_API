c9fdafad906929755b5e656bb86edcc6892510b1
alex <alexliu12@gmail.com>
2019-02-02 17:57:06 -0800

    [prog] adds game start functionality

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ server.listen(PORT, () => console.log('listening on port ' + PORT + ', fellas'))
 app.use(morgan('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-//todo: input correct directory for indexhtml
 app.use(express.static(path.join(__dirname + '/../TWF_Client/client/dist/')));
 
 // app.get('/api/prompt', (req, res) => {
@@ -27,7 +26,7 @@ app.use(express.static(path.join(__dirname + '/../TWF_Client/client/dist/')));
 //   });
 // })
 
-let users = [];
+let players = [];
 
 io.on('connection', (socket) => {
   console.log('new typer joined:', socket.id)
@@ -37,7 +36,7 @@ io.on('connection', (socket) => {
   //listen for joining the room 
   socket.on('joinRoom', (room) => {
     socket.join(room);
-    return socket.emit('joinSuccess', 'You\'ve joined the ' + room);
+    return socket.emit('joinSuccess', 'You\'ve joined ' + room);
   })
 
   //listen for incoming chat
@@ -66,13 +65,36 @@ io.on('connection', (socket) => {
     io.sockets.emit('progress', users);
   });
 
-  socket.on('prompt', () => {
+  // socket.on('prompt', () => {
+  //   axios.get('https://opinionated-quotes-api.gigalixirapp.com/v1/quotes/')
+  //   .then((data) => {
+  //     io.sockets.emit('prompt', data.data);
+  //   })
+  //   .catch((error) => {
+  //     console.log('error getting prompt: ', error);
+  //   });
+  // })
+
+  socket.on('joinGame', (username) => {
+    players.push({
+      username,
+      progress: 0
+    });
+    if (players.length >= 4) {
+      io.sockets.emit('gameStart', 'Game is about to start! Getting prompt...')
+    }
+  })
+
+  socket.on('gameStart', () => {
     axios.get('https://opinionated-quotes-api.gigalixirapp.com/v1/quotes/')
-      .then((data) => {
+    .then((data) => {
       io.sockets.emit('prompt', data.data);
+      io.sockets.emit('gameStartedAll', 'Game has started!');
+      io.to('players').emit('gameStartedPlayers', 'Start typing!');
     })
     .catch((error) => {
       console.log('error getting prompt: ', error);
-    });
+    })
   })
+
 })
